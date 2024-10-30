744db248785916a2228163447f156bea8e33ad92
davileal7 <vissol2024@gmail.com>
2024-10-30 11:13:39 -0300

    up eye

--- JS-login/login_page_script.js
+++ JS-login/login_page_script.js
@@ -106,4 +106,18 @@ const form = {
     passwordRequiredError: () => document.getElementById('password-required-error'),
     recoverPassword: () => document.getElementById('recover-password-button'),
 }
+function togglePassword() {
+    const passwordField = document.getElementById('password');
+    const toggleIcon = document.getElementById('toggleIcon');
+  
+    if (passwordField.type === 'password') {
+      passwordField.type = 'text';
+      toggleIcon.classList.remove('fa-eye-slash');
+      toggleIcon.classList.add('fa-eye');
+    } else {
+      passwordField.type = 'password';
+      toggleIcon.classList.remove('fa-eye');
+      toggleIcon.classList.add('fa-eye-slash');
+    }
+  }
 
